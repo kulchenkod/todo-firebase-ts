abd24be0114488448f18b28d7b9f5fb9d571c3a7
Kulchenko <dmitriykulchenko@gmail.com>
2020-04-27 23:02:21 +0300

    final commit

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC8BnsMZTwYo08jmUXBOGFeu0103n2HdSU',
-  authDomain: 'todos-ec68e.firebaseapp.com',
-  databaseURL: 'https://todos-ec68e.firebaseio.com',
-  projectId: 'todos-ec68e',
-  storageBucket: 'todos-ec68e.appspot.com',
-  messagingSenderId: '744571803358',
-  appId: '1:744571803358:web:439d5be073b5a5ef28944f',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
